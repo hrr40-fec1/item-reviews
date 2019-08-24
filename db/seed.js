7ed684e02d440f132b62b2470a94bd39335d97b5
minmaxdata <kemcadams@gmail.com>
2019-08-24 10:38:38 -0700

    created a file for json output for tests

--- db/seed.js
+++ db/seed.js
@@ -13,12 +13,23 @@ const seeds = {
   Customers: genCustomers(),
   Products: genProducts(),
 };
+
+const removeDb = () => {
+  Reviews.remove({})
+    .then(Customers.remove({}))
+    .then(Products.remove({}))
+    // eslint-disable-next-line no-console
+    .catch((error) => console.log(error));
+};
+
 const insertSeedData = () => {
   Reviews.create(seeds.Reviews.data)
     .then(() => Customers.create(seeds.Customers.data))
     .then(() => Products.create(seeds.Products.data))
     .then(() => db.close())
+    // eslint-disable-next-line no-console
     .catch((error) => console.log(error));
 };
 
+removeDb();
 insertSeedData();
