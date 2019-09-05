cde960c298104bf9ae52448227a11a6e413c3cf4
minmaxdata <kemcadams@gmail.com>
2019-09-04 19:09:11 -0700

    added cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+
 const getReviews = require('../controllers');
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, '/../client/dist')));
+const cors = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+};
+app.use(cors);
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/', (req, res) => res.send('OK'));
 
