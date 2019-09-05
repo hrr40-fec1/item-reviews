4658c33936662d265ae9b1f480fa8885d88fb156
minmaxdata <kemcadams@gmail.com>
2019-09-05 14:43:16 -0700

    moved styles to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
-
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
@@ -15,6 +14,10 @@ module.exports = {
         test: /\.jsx?/,
         include: SRC_DIR,
         use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
