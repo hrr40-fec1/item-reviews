0c2acfa8c207c9556b89f0e4d93e9bbfe0904dc7
minmaxdata <kemcadams@gmail.com>
2019-09-04 17:14:19 -0700

    edits to change the div id

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 // eslint-disable-next-line no-undef
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('Reviews'));
